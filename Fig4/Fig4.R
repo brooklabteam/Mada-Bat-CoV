b78f27cb8ca6de327f9f69ec6e26484495bee098
carabrook <cbrook04@gmail.com>
2021-09-27 09:03:30 -0500

    updates

--- Fig4/Fig4.R
+++ Fig4/Fig4.R
@@ -1,175 +1,119 @@
-rm(list = ls())
+rm(list=ls())
 
-library(plyr)
-library(dplyr)
+
 library(ggplot2)
 library(ggtree)
 library(ape)
 library(ggnewscale)
+library(lubridate)
 
-#load trees and make into multipanel amino acid tree plot
+#make Bayesian timetree from Nobecovirus relaxed molecular clock model
 
-homewd = "/Users/caraebrook/Documents/R/R_repositories/Mada-Bat-CoV/"
-setwd(paste0(homewd, "Fig4")) 
+#first, read in the tree
 
-#load trees
-Stree  <- read.tree(file = paste0(homewd,"Fig4/4-raxml-output/S/Sgene.raxml.supportFBP"))
-Mtree  <- read.tree(file = paste0(homewd,"Fig4/4-raxml-output/M/Mgene.raxml.supportFBP"))
-Etree  <- read.tree(file = paste0(homewd,"Fig4/4-raxml-output/E/Egene.raxml.supportFBP"))
-Ntree  <- read.tree(file = paste0(homewd,"Fig4/4-raxml-output/N/Ngene.raxml.supportFBP"))
+homewd= "/Users/caraebrook/Documents/R/R_repositories/Mada-Bat-CoV/"
+setwd(paste0(homewd, "/Fig4"))
 
-Stree.root<- root(Stree, which(Stree$tip.label == "GammaCoV_NC_010800_1_Turkey_S"))
-Mtree.root<- root(Mtree, which(Mtree$tip.label == "GammaCoV_NC_010800_1_Turkey_M"))
-Etree.root<- root(Etree, which(Etree$tip.label == "GammaCoV_NC_010800_1_Turkey_E"))
-Ntree.root<- root(Ntree, which(Ntree$tip.label == "GammaCoV_NC_010800_1_Turkey_N"))
+tree <- read.annot.beast(file = paste0(homewd, "/Fig4/beast-out/AllNobeco/NobecoRelaxed/AvgNobecoRelaxedNexus"))
+tree <- read.annot.beast(file = paste0(homewd, "/Fig4/beast-out/old/NobecoRelaxed/AvgNobecoRelaxedNexus"))
 
-
-#load metadata
-meta.dat <- read.csv(file = paste0(homewd,"Fig4/amino_acid_metadata_manual.csv"), header = T, stringsAsFactors = F)
-head(meta.dat)
-
-meta.dat$new_label <- NA
-meta.dat$new_label[meta.dat$strain!=""] <- paste0(meta.dat$accession_number[meta.dat$strain!=""], " | ", 
-                                                  meta.dat$strain[meta.dat$strain!=""], " | ",
-                                                  meta.dat$host[meta.dat$strain!=""], " | ",
-                                                  meta.dat$country[meta.dat$strain!=""], " | ",
-                                                  meta.dat$year[meta.dat$strain!=""])
-
-meta.dat$new_label[meta.dat$strain==""] <- paste0(meta.dat$accession_number[meta.dat$strain==""], " | ", 
-                                                  meta.dat$host[meta.dat$strain==""], " | ",
-                                                  meta.dat$country[meta.dat$strain==""], " | ",
-                                                  meta.dat$year[meta.dat$strain==""])
-                                                  
-meta.dat$novel = 0
-meta.dat$novel[meta.dat$country=="Madagascar"] <- 1
-meta.dat$novel <- as.factor(meta.dat$novel)
-meta.dat$bat_host[meta.dat$bat_host==1] <- "bat host"
-meta.dat$bat_host[meta.dat$bat_host==0] <- "non-bat host"
-meta.dat$bat_host <- as.factor(meta.dat$bat_host)
-metaS <- meta.dat
-metaE <- meta.dat
-metaN <- meta.dat
-metaM <- meta.dat
-metaS$tip_label <- paste0(metaS$tip_label, "_S")
-metaE$tip_label <- paste0(metaE$tip_label, "_E")
-metaN$tip_label <- paste0(metaN$tip_label, "_N")
-metaM$tip_label <- paste0(metaM$tip_label, "_M")
-
-Sdat <- data.frame(tip_label=Stree.root$tip.label)
-Sdat <- merge(Sdat, metaS, by="tip_label", all.x = T, sort = F)
-Sdat$old_label <- Sdat$tip_label
-Sdat$tip_label <- Sdat$new_label
-Stree.root$tip.label <- Sdat$tip_label
-
-Edat <- data.frame(tip_label=Etree.root$tip.label)
-Edat <- merge(Edat, metaE, by="tip_label", all.x = T, sort = F)
-Edat$old_label <- Edat$tip_label
-Edat$tip_label <- Edat$new_label
-Etree.root$tip.label <- Edat$tip_label
-
-Mdat <- data.frame(tip_label=Mtree.root$tip.label)
-Mdat <- merge(Mdat, metaM, by="tip_label", all.x = T, sort = F)
-Mdat$old_label <- Mdat$tip_label
-Mdat$tip_label <- Mdat$new_label
-Mtree.root$tip.label <- Mdat$tip_label
+tree$node.label <- round(tree$posterior,2)
+treedat <- cbind.data.frame(tip_name = tree$tip.label)
+treedat$accession_num <- sapply(strsplit(treedat$tip_name, "-"), function(x) x[[1]])
+names(treedat)[names(treedat)=="tip_name"] <- "beast_name"
 
 
-Ndat <- data.frame(tip_label=Ntree.root$tip.label)
-Ndat <- merge(Ndat, metaN, by="tip_label", all.x = T, sort = F)
-Ndat$old_label <- Ndat$tip_label
-Ndat$tip_label <- Ndat$new_label
-Ntree.root$tip.label <- Ndat$tip_label
+#and load data of corresponding tree
+
+dat <- read.csv(file = "fig4_Bayesian_tree_RdRp_metadata.csv", header = T, stringsAsFactors = F)
+dat$collection_date <- as.Date(dat$collection_date)
 
 
-colz = c("Sarbecovirus" = "darkorchid1", "Embecovirus"="darkgoldenrod1", "Gammacoronavirus" = "black", "Hibecovirus" = "royalblue", "Nobecovirus" = "tomato", "Merbecovirus" = "mediumseagreen")
-shapez = c("bat host" =  24, "non-bat host" = 21)
-colz2 = c('1' =  "yellow", '0' = "white")
+#test 
+
+mrsd.dat <- max(dat$collection_date)
+p1 <- ggtree(tree, mrsd=mrsd.dat)  + theme_tree2()  +geom_nodelab()
+
+tree.dat <- p1$data
+node.sub <- dplyr::select(tree.dat, node, x)
+names(node.sub) <-  c("node", "nodetime")
+
+#and 
+head(dat)
+
+dat$clade <- dat$strain
+dat$clade[dat$clade == "GX2018"] <- "BtCoV92 / GX2018" 
+dat$clade[dat$clade == "BtCoV92"] <- "BtCoV92 / GX2018" 
+dat$clade[dat$accession_num == "KU182962"] <- "BtCoV92 / GX2018" 
+dat$clade[dat$host == "Eidolon_helvum" | dat$host == "Rousettus_madagascariensis"] <- "African Eidolon" 
+dat$clade[dat$accession_num=="MG693170"] <- "HKU9"
+dat$clade[dat$host == "Pteropus_rufus" ] <- "Madagascar Pteropus" 
+
+dat.plot <- merge(treedat, dat, by="accession_num", all.x = T, sort=F)
 
 
 
-#first, get the legend.
+dat.plot$new_label[!is.na(dat.plot$strain)] <- paste(dat.plot$accession_num[!is.na(dat.plot$strain)], " | ", 
+                                             dat.plot$strain[!is.na(dat.plot$strain)], " | ", 
+                                             dat.plot$host[!is.na(dat.plot$strain)], " | ",
+                                             dat.plot$country[!is.na(dat.plot$strain)], " | ",
+                                             dat.plot$collection_year[!is.na(dat.plot$strain)])
 
-pleg <- ggtree(Stree.root) %<+% Sdat + 
-  geom_tippoint(aes(color=subgroup, shape=bat_host)) +
-  geom_nodelab(size=1,nudge_x = -.08, nudge_y = .5) +
-  scale_color_manual(values=colz) + 
-  scale_shape_manual(values=shapez) + 
-  new_scale_fill() +
-  geom_tiplab(aes(fill = novel), geom = "label", label.size = 0, alpha=.3, size=1.8, show.legend=F) +
-  scale_fill_manual(values=colz2) + 
-  theme(legend.position = "bottom", legend.title = element_blank(), legend.text = element_text(size=10)) +
-  xlim(c(0,4))
-pleg
-
-legall <- cowplot::get_legend(pleg)
+dat.plot$new_label[is.na(dat.plot$strain)] <- paste(dat.plot$accession_num[is.na(dat.plot$strain)], " | ", 
+                                          dat.plot$host[is.na(dat.plot$strain)], " | ",
+                                          dat.plot$country[is.na(dat.plot$strain)], " | ",
+                                          dat.plot$collection_year[is.na(dat.plot$strain)])
 
 
-pS <- ggtree(Stree.root) %<+% Sdat + 
-  geom_tippoint(aes(fill=subgroup, shape=bat_host), show.legend = F, size=3) +
-  geom_nodelab(size=2.5,nudge_x = -.08, nudge_y = .5) +
-  scale_fill_manual(values=colz) + 
-  scale_shape_manual(values=shapez) + 
-  new_scale_fill() +
-  geom_tiplab(aes(fill = novel), geom = "label", label.size = 0, alpha=.3, size=3.4, show.legend=F) +
-  scale_fill_manual(values=colz2) + 
-  geom_treescale(fontsize=4, x=.7,y=38, linesize = .5) + 
-  #theme(legend.position = c(.2,.85), legend.title = element_blank()) +
-  xlim(c(0,4.5))
-pS
+tree$tip.label <- dat.plot$new_label
+
+dat.sub <- dplyr::select(dat.plot, new_label, collection_date, country, clade)
+head(dat.sub)
+dat.sub$clade <- as.factor(dat.sub$clade)
+
+p2 <-ggtree(tree, mrsd=mrsd.dat) %<+% dat.sub + geom_tippoint(aes(color=clade)) +
+      geom_tiplab(size=3) + geom_nodelab(size=2,nudge_x = -15, nudge_y = .7) +
+      theme_tree2() +
+      theme(legend.position = c(.1,.85),
+            plot.margin = unit(c(2,20,2,3), "lines")) +
+      coord_cartesian(clip = "off")
+
+#and a second node label that is the date for the P ruf and the original
+orig.date <- round(node.sub$nodetime[33],0)
+
+nodePruf <- MRCA(tree, which(tree$tip.label == "KP696747  |  Pteropus_rufus  |  Madagascar  |  2011"),which(tree$tip.label == "OK020087  |  Pteropus_rufus  |  Madagascar  |  2018"))
+nodeall <- MRCA(tree, which(tree$tip.label == "KP696747  |  Pteropus_rufus  |  Madagascar  |  2011"),which(tree$tip.label == "MK211379  |  GX2018  |  Rhinolophus_affinis  |  China  |  2016"))
+orig.date <- round(node.sub$nodetime[nodeall],0)
+#Pruf.date <- round(node.sub$nodetime[nodePruf],0)
+recent.age <- year(mrsd.dat) + yday(mrsd.dat)/365
+Pruf.mean <- round(recent.age-164.7363,0)
+Pruf.uci <- round(recent.age-19.2614,0)
+Pruf.lci <- round(recent.age-513.3403,0)
+Pruf.date <- paste0("~", Pruf.mean, "\n[", Pruf.lci, "-", Pruf.uci, "]")#from FigTree
+new.nodel.lab <- rep(NA, nrow(node.sub))
+#new.nodel.lab[nodeall] <- paste0("~",orig.date)
+new.nodel.lab[nodePruf] <- Pruf.date
+
+dat.sub$clade <- as.character(dat.sub$clade)
+dat.sub$clade[dat.sub$clade=="African Eidolon"] <- "African~italic(Eidolon)"
+dat.sub$clade[dat.sub$clade=="Madagascar Pteropus"] <- "Madagascar~italic(Pteropus)"
+
+p3 <-ggtree(tree, mrsd=mrsd.dat) %<+% dat.sub + geom_tippoint(aes(color=clade), size=3) +
+  geom_tiplab(size=3, nudge_x=5) + geom_nodelab(size=2.5,nudge_x = -15, nudge_y = .7) +
+  geom_nodelab(aes(label=new.nodel.lab), size=3,nudge_x = -60, nudge_y = -1.1,  color="firebrick", fontface=2, geom="label", fill="white") +
+  #theme_tree2() +
+  geom_treescale(fontsize=3, x=1300,y=22, linesize = .5, width=200,label="years") + 
+  scale_color_discrete(labels=c(parse(text="African~italic(Eidolon)"), "BtCoV92 / GX2018", "GCCDC1", "HKU9", parse(text="Madagascar~italic(Pteropus)"))) +
+  theme(legend.position = c(.2,.85), 
+        plot.margin = unit(c(2,20,2,3), "lines")) +
+  coord_cartesian(clip = "off")
 
 
-pE <- ggtree(Etree.root) %<+% Edat + 
-  geom_tippoint(aes(fill=subgroup, shape=bat_host), show.legend = F, size=3) +
-  geom_nodelab(size=2.5,nudge_x = -.08, nudge_y = .5) +
-  scale_fill_manual(values=colz) + 
-  scale_shape_manual(values=shapez) + 
-  new_scale_fill() +
-  geom_tiplab(aes(fill = novel), geom = "label", label.size = 0, alpha=.3, size=3.4, show.legend=F) +
-  scale_fill_manual(values=colz2) + 
-  geom_treescale(fontsize=4, x=.9,y=38, linesize = .5) + 
-  #theme(legend.position = c(.2,.85), legend.title = element_blank()) +
-  xlim(c(0,4.5))
-pE
-
-
-pM <- ggtree(Mtree.root) %<+% Mdat + 
-  geom_tippoint(aes(fill=subgroup, shape=bat_host), show.legend = F, size=3) +
-  geom_nodelab(size=2.5,nudge_x = -.08, nudge_y = .5) +
-  scale_fill_manual(values=colz) + 
-  scale_shape_manual(values=shapez) + 
-  new_scale_fill() +
-  geom_tiplab(aes(fill = novel), geom = "label", label.size = 0, alpha=.3, size=3.4, show.legend=F) +
-  scale_fill_manual(values=colz2) + 
-  geom_treescale(width=.2, fontsize=4, x=.5,y=38, linesize = .5) + 
-  #theme(legend.position = c(.2,.85), legend.title = element_blank()) +
-  xlim(c(0,4.5))
-pM
-
-
-pN <- ggtree(Ntree.root) %<+% Ndat + 
-  geom_tippoint(aes(fill=subgroup, shape=bat_host), show.legend = F, size=3) +
-  geom_nodelab(size=2.5,nudge_x = -.06, nudge_y = .7) +
-  scale_fill_manual(values=colz) + 
-  scale_shape_manual(values=shapez) + 
-  new_scale_fill() +
-  geom_tiplab(aes(fill = novel), geom = "label", label.size = 0, alpha=.3, size=3.4, show.legend=F) +
-  scale_fill_manual(values=colz2) + 
-  geom_treescale(fontsize=4, x=.5,y=38, linesize = .5) + 
-  #theme(legend.position = c(.2,.85), legend.title = element_blank()) +
-  xlim(c(0,4.5))
-pN
-
-
-#and all together
-
-pAminoAcid <- cowplot::plot_grid(pS, pE, pM, pN, nrow = 1, ncol = 4, labels = c("(A)", "(B)", "(C)", "(D)"), label_size = 20)
-
-Fig4 <- cowplot::plot_grid(pAminoAcid, legall, nrow=2, ncol=1, rel_heights = c(1,.1))
 
 ggsave(file = paste0(homewd, "/final-figures/Fig4.png"),
-       plot = Fig4,
        units="mm",  
-       width=180, 
-       height=80, 
+       width=90, 
+       height=60, 
        #limitsize = F,
-       scale=4)#, 
+       scale=3)#, 
+
--- README.md
+++ README.md
@@ -4,9 +4,9 @@ This github repo contains documentation of analyses for the Madagascar bat coron
 
 The repo contains sub-directories with scripts and data to make each figure, as well as directions [here](https://github.com/brooklabteam/Mada-Bat-CoV/blob/main/contig-blast-directions.md) for the pipeline used to determine which samples are "positive" for the pathogen of interest (in this case, CoVs).
 
-Within  the Fig. 3 sub-directory, you will find details on how to build a phylogenetic tree [here](https://github.com/brooklabteam/Mada-Bat-CoV/blob/main/Fig3/Phylo-Tree-Directions.md). This script outlines steps used to build Fig 3A and 3B, as well as the amino acid trees in Fig4.
+Within  the Fig. 3 sub-directory, you will find details on how to build a phylogenetic tree [here](https://github.com/brooklabteam/Mada-Bat-CoV/blob/main/Fig3/Phylo-Tree-Directions.md). This script outlines steps used to build Fig 3A and 3B, as well as the amino acid trees in FigS2.
 
-Additionally, you will find details on how to build a Bayesian phylogenetic tree using [BEAST2](http://www.beast2.org/) within the subfolder for 'FigS2' or linked [here](https://github.com/brooklabteam/Mada-Bat-CoV/blob/main/Fig-S2/beast-tree-intstructions.md). We used this analysis to estimate time to most recent common ancestor for *Nobecoviruses* broadly and the newly-defined Madagascar *Pteropus* lineage specifically.
+Additionally, you will find details on how to build a Bayesian phylogenetic tree using [BEAST2](http://www.beast2.org/) within the subfolder for 'Fig4' or linked [here](https://github.com/brooklabteam/Mada-Bat-CoV/blob/main/Fig4/beast-tree-intstructions.md). We used this analysis to estimate time to most recent common ancestor for *Nobecoviruses* broadly and the newly-defined Madagascar *Pteropus* lineage specifically.
 
 Beyond the details listed above, all scripts to produce each figure should be self-contained and output results to the "final-figures" folder.
 
