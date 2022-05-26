7fd81597e86c1a2f8f06b1bddc9ad01b270628b0
carabrook <cbrook04@gmail.com>
2022-05-26 10:10:26 -0500

    edits to fig5

--- Fig5/Fig5.R
+++ Fig5/Fig5.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(ggplot2)
 library(reshape2)
 
-homewd="/Users/caraebrook/Documents/R/R_repositories/Mada-Bat-CoV"
+homewd="/Users/carabrook/Developer/Mada-Bat-CoV"
 setwd(paste0(homewd, "/Fig5/"))
 
 
